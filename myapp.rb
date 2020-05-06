3bd9deec2a709034cb8d7980e0bfe4e398af5f0a
konaga-k <priests2020@gmail.com>
2020-05-06 16:28:03 +0900

    Add routing

---
+++ myapp.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "sinatra"
+
+get "/" do
+end
+
+get "/memos/new" do
+end
+
+post "/memos" do
+end
+
+get "/memos/:id" do
+end
+
+get "/memos/:id/edit" do
+end
+
+put "/memos/:id" do
+end
+
+delete "/memos/:id" do
+end
